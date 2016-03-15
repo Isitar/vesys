c0d5110b4fe0f9c66b97ae32f209b184a07ff9c4
Pascal Lüscher <luescherpascal@gmail.com>
2016-03-15 17:20:41 +0100

    .

--- bank/src/bank/servlet/BankServlet.java
+++ bank/src/bank/servlet/BankServlet.java
@@ -12,48 +12,44 @@ import bank.Bank;
 import bank.BankDriver;
 
 public class BankServlet extends javax.servlet.http.HttpServlet {
-	
-	static class ServletSI
-	{
+
+	static class ServletSI {
 		BankDriver localDriver;
-			
-		
+
 		private static ServletSI instance;
-		private ServletSI()
-		{
+
+		private ServletSI() {
 			localDriver = new bank.local.Driver();
 			try {
 				localDriver.connect(new String[] { "" });
 			} catch (IOException e) {
 			}
-			
+
 		}
-		public static ServletSI getInstance()
-		{
+
+		public static ServletSI getInstance() {
 			if (instance == null)
 				instance = new ServletSI();
 			return instance;
 		}
-		public Bank getBank()
-		{
+
+		public Bank getBank() {
 			return localDriver.getBank();
 		}
 	}
-	
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-	
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>");
-		
+
 		out.println("<table><tr><th>Number</th></tr>");
 		Set<String> accNumbers = ServletSI.getInstance().getBank().getAccountNumbers();
 		accNumbers.forEach(a -> out.println("<tr><td>" + a + "</td></tr>"));
 		out.println("</table>");
-		
+
 		out.println("</body></html>");
-		System.out.println("<< " + getClass().getName());
 	}
 
 	@Override
