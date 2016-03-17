a42553f71d06d8ed45d4c0bf25ccdd6c9016ae59
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2016-03-17 17:48:55 +0100

    servlet accountcreation working

--- bank/src/bank/servlet/BankServlet.java
+++ bank/src/bank/servlet/BankServlet.java
@@ -2,6 +2,7 @@ package bank.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import java.util.Set;
 
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import bank.Bank;
 import bank.BankDriver;
+import bank.InactiveException;
 
 public class BankServlet extends javax.servlet.http.HttpServlet {
 
@@ -40,6 +42,7 @@ public class BankServlet extends javax.servlet.http.HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<html><body>");
@@ -49,11 +52,54 @@ public class BankServlet extends javax.servlet.http.HttpServlet {
 		accNumbers.forEach(a -> out.println("<tr><td>" + a + "</td></tr>"));
 		out.println("</table>");
 
+		// createaccount
+		out.println("<form action=\"\" method=\"post\">");
+		out.println("<input type=\"hidden\" name=\"action\" value=\"createaccount\">");
+		out.println("Owner: <input type=\"text\" name=\"owner\"><br>");
+		out.println("Balance: <input type=\"text\" name=\"balance\"><br>");
+		out.println("<input type=\"submit\" value=\"Submit\">");
+
 		out.println("</body></html>");
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
+	private void createAccount(HttpServletRequest request) {
+		Bank bank = ServletSI.getInstance().getBank();
+		String owner = getOwner(request);
+		try {
+			String accountNumber = bank.createAccount(owner);
+			double balance = Double.parseDouble(getBalance(request));
+			bank.getAccount(accountNumber).deposit(balance);
+		} catch (IOException | InactiveException e) {
+			e.printStackTrace();
+		}
 	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = getAction(request);
+
+		switch (action) {
+			case "createaccount":
+				createAccount(request);
+				// TODO response.setStatus(HttpServletResponse.SC_CREATED);
+				break;
+			default:
+				break;
+		}
+		
+
+	}
+	
+	private String getOwner(HttpServletRequest request) {
+		return request.getParameterValues("owner")[0];
+	}
+
+	private String getAction(HttpServletRequest request) {
+		return request.getParameterValues("action")[0];
+	}
+
+	private String getBalance(HttpServletRequest request) {
+		return request.getParameterValues("balance")[0];
+	}
+
 }
