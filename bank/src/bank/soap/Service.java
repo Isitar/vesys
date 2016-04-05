0de14c0eb26de5e5a71591d1d09c29d334cb65a6
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2016-04-05 09:13:23 +0200

    service basic conf

--- bank/src/bank/soap/Service.java
+++ bank/src/bank/soap/Service.java
@@ -2,12 +2,29 @@ package bank.soap;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
 @WebService
 public interface Service {
+	
+	@WebMethod
 	List<String> getAccountNumbers();
 
+	@WebMethod
 	String createAccount(@WebParam(name = "owner") String owner);
+	
+	@WebMethod
+	boolean closeAccount(@WebParam(name = "number") String number);
+
+	@WebMethod
+	void deposit(@WebParam(name = "number") String number, @WebParam(name = "amount") double amount);
+
+	@WebMethod
+	void withdraw(@WebParam(name = "number") String number, @WebParam(name = "amount") double amount);
+	
+	@WebMethod
+	void transfer(@WebParam(name = "fromAcc") String fromAcc, @WebParam(name = "toAcc") String toAcc, @WebParam(name = "amount") double amount);
+
 }
